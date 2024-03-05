e0297d14427396bcb0fb816d499599f697c21e78
Fedor Horuzhiy <vailbi94@gmail.com>
2024-03-05 19:52:29 +0300

    beginning of the model

--- gb_recept/recipes/models.py
+++ gb_recept/recipes/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Recipes.Status.PUBLISHED)
+
+
+class Recipes(models.Model):
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
+    title = models.CharField(max_length=255, verbose_name='Название рецепта')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug')
+    content = models.TextField(blank=True, verbose_name='Описание рецепта')
+    steps = models.CharField(max_length=255, verbose_name='Шаги приготовления')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    is_published = models.IntegerField(choices=Status.choices, default=Status.DRAFT, verbose_name='Статус')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts',
+                            verbose_name='Категории')
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name='Тэги')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', default=None, null=True, blank=True,
+                              verbose_name='Фото')
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
+    slug = models.CharField(max_length=100, unique=True, db_index=True)
